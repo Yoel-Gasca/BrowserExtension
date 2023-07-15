38e07e27e8c2ae9e18d5595744c087f2d366679b
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-07-14 23:07:34 -0600

    Call an API & use localStorage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,106 @@
-//1
-// form fields
-// results divs
+// Campos del formulario
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+const apiKey = document.querySelector('.api-key');
+
+// Resultados
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon-usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.my-region');
+const clearBtn = document.querySelector('.clear-btn');
+
+// Eventos para el formulario
+form.addEventListener('submit', (e) => handleSubmit(e));
+clearBtn.addEventListener('click', (e) => reset(e));
+init();
+
+// Funciones de init y reset
+function init() {
+	//si hay algo en localStorage, recójalo
+	const storedApiKey = localStorage.getItem('apiKey');
+	const storedRegion = localStorage.getItem('regionName');
+
+	//establecer el icono en verde genérico
+	//todo
+
+	if (storedApiKey === null || storedRegion === null) {
+		//si no tenemos las claves, mostrar el formulario
+		form.style.display = 'block';
+		results.style.display = 'none';
+		loading.style.display = 'none';
+		clearBtn.style.display = 'none';
+		errors.textContent = '';
+	} else {
+        //si hemos guardado claves / regiones en localStorage, mostrar los resultados cuando se cargan
+        displayCarbonUsage(storedApiKey, storedRegion);
+		results.style.display = 'none';
+		form.style.display = 'none';
+		clearBtn.style.display = 'block';
+	}
+};
+
+function reset(e) {
+	e.preventDefault();
+	//borrar almacenamiento local solo para la región
+	localStorage.removeItem('regionName');
+	init();
+}
+
+//Manejar el envío del formulario
+function handleSubmit(e) {
+	e.preventDefault();
+	setUpUser(apiKey.value, region.value);
+}
+
+// Configurar el usuario
+function setUpUser(apiKey, regionName) {
+	localStorage.setItem('apiKey', apiKey);
+	localStorage.setItem('regionName', regionName);
+	loading.style.display = 'block';
+	errors.textContent = '';
+	clearBtn.style.display = 'block';
+	//make initial call
+	displayCarbonUsage(apiKey, regionName);
+}
 
 //6
-//call the API
+//Llamar a la API
+import axios from '../node_modules/axios';
 
-//5
-//set up user's api key and region
+async function displayCarbonUsage(apiKey, region) {
+	try {
+		await axios
+			.get('https://api.co2signal.com/v1/latest', {
+				params: {
+					countryCode: region,
+				},
+				headers: {
+					'auth-token': apiKey,
+				},
+			})
+			.then((response) => {
+				let CO2 = Math.floor(response.data.data.carbonIntensity);
 
-//4
-// handle form submission
+				//calculateColor(CO2);
 
-//3 initial checks
+				loading.style.display = 'none';
+				form.style.display = 'none';
+				myregion.textContent = region;
+				usage.textContent =
+					Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+				fossilfuel.textContent =
+					response.data.data.fossilFuelPercentage.toFixed(2) +
+					'% (percentage of fossil fuels used to generate electricity)';
+				results.style.display = 'block';
+			});
+	} catch (error) {
+		console.log(error);
+		loading.style.display = 'none';
+		results.style.display = 'none';
+		errors.textContent = 'Lo sentimos, no tenemos datos para la región que ha solicitado';
+	}
+}
 
-//2
-// set listeners and start app
