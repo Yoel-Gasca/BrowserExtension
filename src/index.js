deee93677c2a59c9f195d75ec9b54b47d36437b1
Yoel-Gasca <83617933+Yoel-Gasca@users.noreply.github.com>
2023-07-18 21:49:26 -0600

    Background Tasks and Performance

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-// Campos del formulario
+import axios from 'axios';
+//1
+// form fields
 const form = document.querySelector('.form-data');
 const region = document.querySelector('.region-name');
 const apiKey = document.querySelector('.api-key');
 
-// Resultados
+// results
 const errors = document.querySelector('.errors');
 const loading = document.querySelector('.loading');
 const results = document.querySelector('.result-container');
@@ -12,18 +14,89 @@ const fossilfuel = document.querySelector('.fossil-fuel');
 const myregion = document.querySelector('.my-region');
 const clearBtn = document.querySelector('.clear-btn');
 
-// Eventos para el formulario
-form.addEventListener('submit', (e) => handleSubmit(e));
-clearBtn.addEventListener('click', (e) => reset(e));
-init();
+function calculateColor(value) {
+	let co2Scale = [0, 150, 600, 750, 800];
+	let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
 
-// Funciones de init y reset
+	let closestNum = co2Scale.sort((a, b) => {
+		return Math.abs(a - value) - Math.abs(b - value);
+	})[0];
+	console.log(value + ' is closest to ' + closestNum);
+	let num = (element) => element > closestNum;
+	let scaleIndex = co2Scale.findIndex(num);
+
+	let closestColor = colors[scaleIndex];
+	console.log(scaleIndex, closestColor);
+
+	chrome.runtime.sendMessage({ action: 'updateIcon', value: { color: closestColor } });
+}
+
+//6
+//call the API
+async function displayCarbonUsage(apiKey, region) {
+	try {
+		await axios
+			.get('https://api.co2signal.com/v1/latest', {
+				params: {
+					countryCode: region,
+				},
+				headers: {
+					'auth-token': apiKey,
+				},
+			})
+			.then((response) => {
+				//let CO2 = Math.floor(response.data.data.carbonIntensity);
+
+				calculateColor(CO2);
+
+				loading.style.display = 'none';
+				form.style.display = 'none';
+				myregion.textContent = region;
+				usage.textContent =
+					Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+				fossilfuel.textContent =
+					response.data.data.fossilFuelPercentage.toFixed(2) +
+					'% (percentage of fossil fuels used to generate electricity)';
+				results.style.display = 'block';
+			});
+	} catch (error) {
+		console.log(error);
+		loading.style.display = 'none';
+		results.style.display = 'none';
+		errors.textContent = 'Lo sentimos, no tenemos datos para la región que ha solicitado';
+	}
+}
+
+//5
+//set up user's api key and region
+function setUpUser(apiKey, regionName) {
+	localStorage.setItem('apiKey', apiKey);
+	localStorage.setItem('regionName', regionName);
+	loading.style.display = 'block';
+	errors.textContent = '';
+	clearBtn.style.display = 'block';
+	//make initial call
+	displayCarbonUsage(apiKey, regionName);
+}
+//4
+// handle form submission
+function handleSubmit(e) {
+	e.preventDefault();
+	setUpUser(apiKey.value, region.value);
+}
+//3 initial checks
 function init() {
 	//si hay algo en localStorage, recójalo
 	const storedApiKey = localStorage.getItem('apiKey');
 	const storedRegion = localStorage.getItem('regionName');
 
 	//establecer el icono en verde genérico
+	chrome.runtime.sendMessage({
+		action: 'updateIcon',
+			value: {
+				color: 'green',
+			},
+	});
 	//todo
 
 	if (storedApiKey === null || storedRegion === null) {
@@ -49,58 +122,8 @@ function reset(e) {
 	init();
 }
 
-//Manejar el envío del formulario
-function handleSubmit(e) {
-	e.preventDefault();
-	setUpUser(apiKey.value, region.value);
-}
-
-// Configurar el usuario
-function setUpUser(apiKey, regionName) {
-	localStorage.setItem('apiKey', apiKey);
-	localStorage.setItem('regionName', regionName);
-	loading.style.display = 'block';
-	errors.textContent = '';
-	clearBtn.style.display = 'block';
-	//make initial call
-	displayCarbonUsage(apiKey, regionName);
-}
-
-//6
-//Llamar a la API
-import axios from '../node_modules/axios';
-
-async function displayCarbonUsage(apiKey, region) {
-	try {
-		await axios
-			.get('https://api.co2signal.com/v1/latest', {
-				params: {
-					countryCode: region,
-				},
-				headers: {
-					'auth-token': apiKey,
-				},
-			})
-			.then((response) => {
-				let CO2 = Math.floor(response.data.data.carbonIntensity);
-
-				//calculateColor(CO2);
-
-				loading.style.display = 'none';
-				form.style.display = 'none';
-				myregion.textContent = region;
-				usage.textContent =
-					Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
-				fossilfuel.textContent =
-					response.data.data.fossilFuelPercentage.toFixed(2) +
-					'% (percentage of fossil fuels used to generate electricity)';
-				results.style.display = 'block';
-			});
-	} catch (error) {
-		console.log(error);
-		loading.style.display = 'none';
-		results.style.display = 'none';
-		errors.textContent = 'Lo sentimos, no tenemos datos para la región que ha solicitado';
-	}
-}
-
+//2
+// set listeners and start app
+form.addEventListener('submit', (e) => handleSubmit(e));
+clearBtn.addEventListener('click', (e) => reset(e));
+init();
